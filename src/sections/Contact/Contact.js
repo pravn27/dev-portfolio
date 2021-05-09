f7d5d4be62366f381ba07004fc0b60a9ef5a69c6
pravn27 <praveen.sri27@gmail.com>
2021-05-09 09:23:10 +0530

    Adding Contact section

--- src/sections/Contact/Contact.js
+++ src/sections/Contact/Contact.js
@@ -1,10 +1,36 @@
 import React from "react";
 import CustomCard from "../../components/CustomCard/CustomCard";
+import { LeftSideContainer } from "./Styled";
 
 const Contact = () => {
   return (
-    <CustomCard title="Contact">
-      <h6>Card body about Contacts</h6>
+    <CustomCard title="Get in touch">
+      <div className="row">
+        <LeftSideContainer className="col-md-4">
+          <img
+            className="mx-auto d-block"
+            src="https://avatars.githubusercontent.com/u/17290429?v=4"
+            alt="dev-pic"
+            width="150"
+            height="150"
+          />
+          <button
+            className="btn btn-light mr-2"
+            onClick={() => window.open("https://github.com/pravn27")}
+          >
+            <i className="fab fa-github fa-lg"></i>
+          </button>
+          <button
+            className="btn btn-light mr-2"
+            onClick={() =>
+              window.open("https://www.linkedin.com/in/praveen-s-157b365a/")
+            }
+          >
+            <i className="fab fa-linkedin fa-lg"></i>
+          </button>
+        </LeftSideContainer>
+        <div className="col-md-8">Lets connect & want to discuss together</div>
+      </div>
     </CustomCard>
   );
 };
--- src/sections/Contact/Styled.js
+++ src/sections/Contact/Styled.js
@@ -1,1 +1,14 @@
 import styled from "styled-components";
+
+export const LeftSideContainer = styled.div`
+  text-align: center;
+  img {
+    border-radius: 50%;
+    padding: 2px;
+    border: 2px solid #00bcd4;
+    margin-bottom: 10px;
+  }
+  button {
+    border: 1px solid lightgray;
+  }
+`;
