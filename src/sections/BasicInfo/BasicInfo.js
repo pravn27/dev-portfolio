87929ec7712857b9e4e94e27056a6bff35e06877
pravn27 <praveen.sri27@gmail.com>
2022-10-04 19:51:16 +0530

    updated basic info, align with present roles & experience

--- src/sections/BasicInfo/BasicInfo.js
+++ src/sections/BasicInfo/BasicInfo.js
@@ -1,46 +1,61 @@
 import React from "react";
 import { MainContainer, LinkContainer, DevInfoContainer } from "./Styled";
 
+const jsonResponse = {
+  fullName: "Praveen Shekarappa",
+  email: "praveen.sri27@gmail.com",
+  role: "Senior Software Engineer | ReactJS Developer | UI/UX Web/Mobile app developer",
+  description:
+    "A result-orientated professional over 6+ Year's experience in UI Software Development in web and mobile application development with frontend cutting edge technologies & extensive experience on ReactJS & JavaScript in building web application interface.",
+  avatarImgUrl: "https://avatars.githubusercontent.com/u/17290429?v=4",
+  githubUrl: "https://github.com/pravn27",
+  linkedinUrl: "https://www.linkedin.com/in/praveen-s-157b365a/",
+  downloadCvUrl:
+    "https://drive.google.com/file/d/1JaT7xaI82I0vJ7jzpycR9nlN0TSTPDvK/view?usp=sharing",
+};
+
 const BasicInfo = () => {
+  const {
+    fullName,
+    email,
+    role,
+    description,
+    avatarImgUrl,
+    githubUrl,
+    linkedinUrl,
+    downloadCvUrl,
+  } = jsonResponse;
+
+  const openInNewTab = (url) => window.open(url);
+  const openMailTo = () => (window.location = `mailto:${email}`);
+
   return (
     <MainContainer>
       <LinkContainer className="mb-3">
         <div className="BtnContainer">
-          <button
-            className="btn btn-light mr-2"
-            onClick={() => (window.location = "mailto:praveen.sri27@gmail.com")}
-          >
+          <button className="btn btn-light mr-2" onClick={() => openMailTo()}>
             <i className="fas fa-envelope fa-lg"></i>
           </button>
           <button
             className="btn btn-light mr-2"
-            onClick={() => window.open("https://github.com/pravn27")}
+            onClick={() => openInNewTab(githubUrl)}
           >
             <i className="fab fa-github fa-lg"></i>
           </button>
           <button
             className="btn btn-light mr-2"
-            onClick={() =>
-              window.open("https://www.linkedin.com/in/praveen-s-157b365a/")
-            }
+            onClick={() => openInNewTab(linkedinUrl)}
           >
             <i className="fab fa-linkedin fa-lg"></i>
           </button>
         </div>
         <div className="BtnContainer">
-          <button
-            className="btn btn-light mr-2"
-            onClick={() => (window.location = "mailto:praveen.sri27@gmail.com")}
-          >
+          <button className="btn btn-light mr-2" onClick={() => openMailTo()}>
             <i className="fas fa-paper-plane"></i> HIRE ME
           </button>
           <button
             className="btn btn-light"
-            onClick={() =>
-              window.open(
-                "https://drive.google.com/file/d/1o7H5oGPH4Q8YtWFbu7XjjDumifNfA_CW/view?usp=sharing"
-              )
-            }
+            onClick={() => openInNewTab(downloadCvUrl)}
           >
             <i className="fas fa-download"></i> DOWNLOAD CV
           </button>
@@ -49,24 +64,16 @@ const BasicInfo = () => {
       <DevInfoContainer>
         <img
           className="img-thumbnail mx-auto d-block"
-          src="https://avatars.githubusercontent.com/u/17290429?v=4"
+          src={avatarImgUrl}
           alt="dev-pic"
           width="150"
           height="150"
         />
-        <h1 className="text-center text-white">Praveen Shekarappa</h1>
+        <h1 className="text-center text-white">{fullName}</h1>
         <p className="lead text-center text-white">
-          <strong>
-            Senior Software Engineer | ReactJS Developer | UI/UX Web/Mobile app
-            developer
-          </strong>
+          <strong>{role}</strong>
         </p>
-        <p className="lead text-center text-white">
-          A result-orientated professional over 5.7 Year’s experience in UI
-          Software Development in web and mobile application development with
-          frontend cutting edge technologies & extensive experience on ReactJS &
-          JavaScript in building web application interface.
-        </p>
+        <p className="lead text-center text-white">{description}</p>
       </DevInfoContainer>
     </MainContainer>
   );
